3bab230d5be69f9e15bdde47515edcfd430806ac
NH <mnh4140@gmail.com>
2025-04-01 21:16:51 +0900

    env: 프로젝트 경로 변경

---
+++ Codebase-CalculatorTests/Codebase_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Codebase_CalculatorTests.swift
+//  Codebase-CalculatorTests
+//
+//  Created by NH on 4/1/25.
+//
+
+import Testing
+@testable import Codebase_Calculator
+
+struct Codebase_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
