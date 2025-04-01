42179fb6e199bae7dd3f4bb9d9a3a66894e3aef9
LEMONY <jin.lee1028@gmail.com>
2025-04-01 12:50:27 +0900

    Lv.2 구현 중간 저장

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,15 +10,19 @@ import UIKit
 class ViewController: UIViewController {
     
     let label = UILabel()
+    let sevenButton: UIButton = UIButton()
+    let eightButton: UIButton = UIButton()
+    let nineButton: UIButton = UIButton()
+    let plusButton: UIButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         screenNumberUI()
+        sevenButtonUI()
         
     }
-    
     private func screenNumberUI() {
         view.backgroundColor = .black
         label.text = "12345"
@@ -34,7 +38,20 @@ class ViewController: UIViewController {
             label.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
             label.heightAnchor.constraint(equalToConstant: 100)
         ])
+    }
+    
+    private func sevenButtonUI() {
+        sevenButton.titleLabel?.text = "7"
+        sevenButton.titleLabel?.textColor = .white
+        sevenButton.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+        sevenButton.titleLabel?.font = .systemFont(ofSize: 30)
+        sevenButton.layer.cornerRadius = 40
         
+        view.addSubview(sevenButton)
+        sevenButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            sevenButton.heightAnchor.constraint(equalToConstant: 80),
+            sevenButton.widthAnchor.constraint(equalToConstant: 80)
+        ])
     }
 }
-
